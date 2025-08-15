045fc33ac265a16a819dc681e09bc68cbdf754bf
Petrina <petrinanjora@gmail.com>
2025-08-15 12:32:50 +0300

    Add files

---
+++ q1.py
@@ -0,0 +1,5 @@
+name = 'Petrina'
+age = 16
+favourite_animal = 'Axolotl'
+
+print('My name is', name, 'I am', age, 'years old and, my favourite animal is', favourite_animal + '.') 
---
+++ q2.py
@@ -0,0 +1,4 @@
+fruits_list = ['apple', 'banana', 'cherry', 'date', 'grape']
+print(fruits_list)
+print(fruits_list[0])
+print(fruits_list[4])
